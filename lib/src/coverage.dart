d5877911afbca4ed4c7729c28924c1f351e05391
Cédric Belin <cedric@belin.io>
2017-01-05 13:29:05 +0100

    Updated the Coverage dependency

--- lib/src/coverage.dart
+++ lib/src/coverage.dart
@@ -16,22 +16,22 @@ class Coverage {
   List<String> reportOn;
 
   /// Runs the specified [script] and returns its coverage data as LCOV format.
-  String collect(File script, {List<String> arguments, Duration timeout}) {
+  String collect(File script, {List<String> arguments, bool checked = true, Duration timeout}) {
     var coverage;
     new Future<String>
-      .sync(() => collectAsync(script, arguments: arguments, timeout: timeout))
+      .sync(() => collectAsync(script, arguments: arguments, checked: checked, timeout: timeout))
       .then((cov) => coverage = cov);
 
     return coverage;
   }
 
   /// Runs asynchronously the specified [script] and returns its coverage data as LCOV format.
-  Future<String> collectAsync(File script, {List<String> arguments, Duration timeout}) async {
+  Future<String> collectAsync(File script, {List<String> arguments, bool checked = true, Duration timeout}) async {
     assert(script != null);
     if (!await script.exists())
       throw new ArgumentError.value(script, 'script', 'The specified file does not exist.');
 
-    var coverage = await runAndCollect(script.path, scriptArgs: arguments);
+    var coverage = await runAndCollect(script.path, checked: checked, scriptArgs: arguments, timeout: timeout);
     var resolver = new Resolver(packagesPath: packagesPath, sdkRoot: sdkDir.path);
     return await new LcovFormatter(resolver, basePath: basePath, reportOn: reportOn).format(coverage);
   }
