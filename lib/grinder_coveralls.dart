48500c331c2b97cc504536e1625ef52e441f5613
Cédric Belin <cedric@belin.io>
2017-01-20 01:08:22 +0100

    Externalized the Coveralls client

--- lib/grinder_coveralls.dart
+++ lib/grinder_coveralls.dart
@@ -8,60 +8,29 @@ import 'package:http/http.dart' as http;
 import 'package:coverage/coverage.dart';
 import 'package:grinder/grinder.dart';
 
-part 'src/coverage.dart';
-part 'src/coveralls_client.dart';
-part 'src/coveralls_formatter.dart';
-part 'src/lcov_parser.dart';
+part 'src/client.dart';
+part 'src/collector.dart';
 
 /// Runs the specified [script] with optional [arguments].
 /// Uploads the collected coverage data to the Coveralls service.
-void collectAndUploadCoverage(dynamic script, {List<String> arguments}) {
-  _runSync(() => collectAndUploadCoverageAsync(script, arguments: arguments));
-}
-
-/// Runs asynchronously the specified [script] with optional [arguments].
-/// Uploads the collected coverage data to the Coveralls service.
-Future collectAndUploadCoverageAsync(dynamic script, {List<String> arguments}) async {
+Future collectAndUploadCoverage(dynamic script, {List<String> arguments}) async {
   var path = script is FilePath ? script : new FilePath(script);
-  var coverage = await new Coverage().collect(path.asFile, arguments: arguments);
-  await new CoverallsClient().upload(coverage);
+  var coverage = await new Collector().collect(path.asFile, arguments: arguments);
+  return new Client().upload(coverage);
 }
 
 /// Runs the specified [script] and saves its coverage data as LCOV format.
 ///
 /// The [output] path specifies the destination file.
 /// The [arguments] list provides the optional script arguments.
-void collectCoverage(dynamic script, String output, {List<String> arguments}) {
-  _runSync(() => collectCoverageAsync(script, output, arguments: arguments));
-}
-
-/// Runs asynchronously the specified [script] and saves its coverage data as LCOV format.
-///
-/// The [output] path specifies the destination file.
-/// The [arguments] list provides the optional script arguments.
-Future collectCoverageAsync(dynamic script, String output, {List<String> arguments}) async {
+Future collectCoverage(dynamic script, String output, {List<String> arguments}) async {
   var path = script is FilePath ? script : new FilePath(script);
-  var coverage = await new Coverage().collect(path.asFile, arguments: arguments);
-  await getFile(output).writeAsString(coverage);
+  var coverage = await new Collector().collect(path.asFile, arguments: arguments);
+  return getFile(output).writeAsString(coverage);
 }
 
 /// Uploads the specified code [coverage] report in LCOV format to the Coveralls service.
-void uploadCoverage(dynamic coverage) {
-  _runSync(() => uploadCoverageAsync(coverage));
-}
-
-/// Uploads asynchronously the specified code [coverage] report in LCOV format to the Coveralls service.
-Future uploadCoverageAsync(dynamic coverage) async {
+Future uploadCoverage(dynamic coverage) async {
   var path = coverage is FilePath ? coverage : new FilePath(coverage);
-  await new CoverallsClient().uploadFile(path.asFile);
-}
-
-/// Returns the result of immediately calling [computation].
-dynamic _runSync(computation()) {
-  var result;
-  new Future.sync(() => computation())
-    .then((res) => result = res)
-    .catchError((err) => throw err);
-
-  return result;
+  return new Client().uploadFile(path.asFile);
 }
--- lib/src/collector.dart
+++ lib/src/collector.dart
@@ -1,10 +1,10 @@
 part of grinder_coveralls;
 
 /// Collects the coverage data of a Dart script as [LCOV](http://ltp.sourceforge.net/coverage/lcov.php) format.
-class Coverage {
+class Collector {
 
   /// Creates a new coverage collector.
-  Coverage({this.basePath, this.packagesPath = '.packages', this.reportOn = const ['lib/']});
+  Collector({this.basePath, this.packagesPath = '.packages', List<String> reportOn}) : reportOn = reportOn ?? ['lib'];
 
   /// If provided, paths are reported relative to that path.
   String basePath;
@@ -13,13 +13,13 @@ class Coverage {
   String packagesPath;
 
   /// If provided, coverage report output is limited to files prefixed with one of the paths included.
-  List<String> reportOn;
+  final List<String> reportOn;
 
   /// Runs the specified [script] and returns its coverage data as LCOV format.
   Future<String> collect(File script, {List<String> arguments, bool checked = true, Duration timeout}) async {
-    var coverage = await collectHitmap(script, arguments: arguments, checked: checked, timeout: timeout);
+    var hitmap = await collectHitmap(script, arguments: arguments, checked: checked, timeout: timeout);
     var resolver = new Resolver(packagesPath: packagesPath, sdkRoot: sdkDir.path);
-    return await new LcovFormatter(resolver, basePath: basePath, reportOn: reportOn).format(coverage);
+    return new LcovFormatter(resolver, basePath: basePath, reportOn: reportOn).format(hitmap);
   }
 
   /// Runs the specified [script] and returns its coverage data as hitmap.
@@ -28,6 +28,7 @@ class Coverage {
     if (!await script.exists())
       throw new ArgumentError.value(script, 'script', 'The specified file does not exist.');
 
-    return await runAndCollect(script.path, checked: checked, scriptArgs: arguments, timeout: timeout);
+    var report = await runAndCollect(script.path, checked: checked, scriptArgs: arguments, timeout: timeout);
+    return report.containsKey('coverage') ? createHitmap(report['coverage']) : const {};
   }
 }
