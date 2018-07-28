34ee20baba07ea3e504c94c770a4e1aa875c130d
Cédric Belin <cedric@belin.io>
2018-07-29 01:52:51 +0200

    Updated the build system

--- tool/grind.dart
+++ tool/grind.dart
@@ -2,15 +2,16 @@ import 'dart:async';
 import 'package:grinder/grinder.dart';
 import 'package:grinder_coveralls/grinder_coveralls.dart';
 
-/// The list of source directories.
-const List<String> _sources = const ['lib', 'test', 'tool'];
-
 /// Starts the build system.
-Future main(List<String> args) => grind(args);
+Future<void> main(List<String> args) => grind(args);
 
 /// Deletes all generated files and reset any saved state.
 @Task('Delete the generated files')
-void clean() => defaultClean();
+void clean() {
+  defaultClean();
+  ['.dart_tool/build', 'doc/api', webDir.path].map(getDir).forEach(delete);
+  FileSet.fromDir(getDir('var'), pattern: '*.{info,json}').files.forEach(delete);
+}
 
 /// Uploads the code coverage report.
 @Task('Upload the code coverage')
@@ -19,16 +20,28 @@ Future coverage() => uploadCoverage('var/lcov.info');
 
 /// Builds the documentation.
 @Task('Build the documentation')
-void doc() => DartDoc.doc();
+void doc() {
+  DartDoc.doc();
+  run('mkdocs', arguments: ['build']);
+}
 
 /// Fixes the coding standards issues.
 @Task('Fix the coding issues')
-void fix() => DartFmt.format(_sources);
+void fix() => DartFmt.format(existingSourceDirs, lineLength: 200);
 
 /// Performs static analysis of source code.
 @Task('Perform the static analysis')
-void lint() => Analyzer.analyze(_sources);
+void lint() => Analyzer.analyze(existingSourceDirs);
 
 /// Runs all the test suites.
 @Task('Run the tests')
 Future test() => collectCoverage('test/all.dart', 'var/lcov.info');
+
+/// Upgrades the project to the latest revision.
+@Task('Upgrade the project')
+void upgrade() {
+  run('git', arguments: ['reset', '--hard']);
+  run('git', arguments: ['fetch', '--all', '--prune']);
+  run('git', arguments: ['pull', '--rebase']);
+  Pub.upgrade();
+}
