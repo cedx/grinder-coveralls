b054f09ef0931d8da00f2ace0f8f4a802da1697c
Cédric Belin <cedric@belin.io>
2019-04-23 12:55:22 +0200

    Fixed the `collectCoverage` function

--- lib/grinder_coveralls.dart
+++ lib/grinder_coveralls.dart
@@ -11,43 +11,48 @@ import 'package:crypto/crypto.dart';
 import 'package:grinder/grinder.dart' hide ProcessException;
 
 part 'src/collector.dart';
-part 'src/functions.dart';
 
 /// Profiles the specified [source] file or directory containing test files, and returns the collected coverage data as LCOV format.
 ///
+/// If [source] is a directory, it will be scanned according to the value of the [recurse] parameter for files matching the specified [pattern].
 /// If [basePath] is provided, paths are reported relative to this path.
 /// If [environment] is provided, its entries will be injected as compile-time constants.
 /// If [packagesFile] is provided, it will be used as path to the `.packages` specification file.
-/// If [saveAs] is provided, coverage report output will be written to a file at the given path.
 /// If [reportOn] is provided, coverage report output is limited to files prefixed with one of the paths included.
+/// If [saveAs] is provided, coverage report output will be written to a file at the given path.
 /// If [timeout] is provided, a `TimeoutException` is thrown when the coverage collection exceeds the given duration.
+///
+/// The [observatoryAddress] and [observatoryPort] values indicate where the VM service is bound,
+/// while the [silent] value indicates whether to silent the collector output.
 Future<String> collectCoverage(FileSystemEntity source, {
   String basePath,
   Map<String, String> environment,
-  String observatoryAddress,
+  observatoryAddress,
   int observatoryPort = 8181,
-  Object packagesFile,
+  packagesFile,
+  String pattern = '*_test.dart',
+  bool recurse = true,
   List<String> reportOn,
-  Object saveAs,
+  saveAs,
   bool silent = false,
   Duration timeout
 }) async {
-  final collect = source is Directory ? collectFromDirectory : collectFromFile;
-  final coverage = await collect(
-    source,
-    basePath: basePath,
-    environment: environment,
-    observatoryAddress: observatoryAddress,
-    observatoryPort: observatoryPort,
-    packagesFile: packagesFile,
-    reportOn: reportOn,
-    silent: silent,
-    timeout: timeout
-  );
+  final address = observatoryAddress is InternetAddress ? observatoryAddress : InternetAddress(observatoryAddress ?? InternetAddress.loopbackIPv4.address);
+  final collector = Collector(observatoryAddress: address, observatoryPort: observatoryPort)
+    ..basePath = basePath
+    ..environment = environment
+    ..packagesFile = FilePath(packagesFile ?? '.packages').asFile
+    ..reportOn = reportOn
+    ..silent = silent
+    ..timeout = timeout;
+
+  final coverage = source is Directory
+    ? await collector.collectFromFiles(FileSet.fromDir(source, pattern: pattern, recurse: recurse).files)
+    : await collector.collectFromFile(source);
 
   if (saveAs != null) await FilePath(saveAs).asFile.writeAsString(coverage);
   return coverage;
 }
 
-/// Uploads the specified code coverage [report], in Clover or LCOV format, to the Coveralls service.
+/// Uploads the specified code coverage [report] to the Coveralls service.
 Future<void> uploadCoverage(String report, {Configuration configuration, Uri endPoint}) => Client(endPoint).upload(report, configuration);
