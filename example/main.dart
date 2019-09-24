0cd088b90ea9f5f1f9d9b442abbb55062678ee83
Cédric Belin <cedric@belin.io>
2019-09-24 17:38:58 +0200

    Updated the example code

--- example/main.dart
+++ example/main.dart
@@ -3,11 +3,11 @@ import 'package:grinder_coveralls/grinder_coveralls.dart' as coveralls;
 
 @Task('Collects and uploads the coverage data in one pass')
 Future<void> collectAndUploadCoverage() async =>
-  coveralls.uploadCoverage(await coveralls.collectCoverage(getDir('test'), reportOn: [libDir.path]));
+  coveralls.uploadCoverage(await coveralls.collectCoverage('test/**_test.dart', reportOn: [libDir.path]));
 
 @Task('Collects the coverage data and saves it as LCOV format')
 Future<void> collectCoverage() =>
-  coveralls.collectCoverage(getDir('test'), reportOn: [libDir.path], saveAs: 'path/to/lcov.info');
+  coveralls.collectCoverage('test/**_test.dart', reportOn: [libDir.path], saveAs: 'path/to/lcov.info');
 
 @Task('Uploads the LCOV coverage report to the Coveralls service')
 Future<void> uploadCoverage() async =>
