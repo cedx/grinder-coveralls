a5752dbf9be0b5b8451c75251620eeec9eaf7fcb
Cédric Belin <cedric@belin.io>
2019-09-24 19:52:03 +0200

    Updated the example code

--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,14 @@
 import 'package:grinder/grinder.dart';
-import 'package:grinder_coveralls/grinder_coveralls.dart' as coveralls;
+import 'package:grinder_coveralls/grinder_coveralls.dart';
 
 @Task('Collects and uploads the coverage data in one pass')
-Future<void> collectAndUploadCoverage() async =>
-  coveralls.uploadCoverage(await coveralls.collectCoverage('test/**_test.dart', reportOn: [libDir.path]));
+Future<void> collectAndUploadCodeCoverage() async =>
+  uploadCoverage(await collectCoverage('test/**_test.dart', reportOn: [libDir.path]));
 
 @Task('Collects the coverage data and saves it as LCOV format')
-Future<void> collectCoverage() =>
-  coveralls.collectCoverage('test/**_test.dart', reportOn: [libDir.path], saveAs: 'path/to/lcov.info');
+Future<void> collectCodeCoverage() =>
+  collectCoverage('test/**_test.dart', reportOn: [libDir.path], saveAs: 'path/to/lcov.info');
 
 @Task('Uploads the LCOV coverage report to the Coveralls service')
-Future<void> uploadCoverage() async =>
-  coveralls.uploadCoverage(await getFile('path/to/lcov.info').readAsString());
+Future<void> uploadCoverageReport() async =>
+  uploadCoverage(await getFile('path/to/lcov.info').readAsString());
